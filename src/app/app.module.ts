2d9d0ebd881d9bd805c58ba23bb8a14b21b263f8
Mukesh <mukesh.rajasekar@gmail.com>
2020-03-15 11:50:03 +0530

    Commit -- CSS Effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
   ],
   imports: [
     BrowserModule,
